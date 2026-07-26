f292e738483e5562c4c2f8bcd31234def86b6a2c
ScottStaschke <Scott.Staschke@gmail.com>
2026-07-26 17:54:01 -0400

    Add bounded Windows input diagnostics

--- rkvm-client/src/windows_daemon/writer.rs
+++ rkvm-client/src/windows_daemon/writer.rs
@@ -5,6 +5,10 @@ use rkvm_net::Update;
 
 use async_trait::async_trait;
 use std::io::Error;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+const EVENT_LOG_LIMIT: usize = 20;
+static FORWARDED_EVENTS: AtomicUsize = AtomicUsize::new(0);
 
 pub struct ClientWriter<W> {
     writer: W,
@@ -21,7 +25,15 @@ where W: RkvmWriter + Send {
 impl<W> EventWriter for ClientWriter<W>
 where W: RkvmWriter + Send {
     async fn event(&mut self, id: usize, event: Event) -> Result<(), Error> {
-            tracing::debug!("Forward {:?}", event);
+            let event_number = FORWARDED_EVENTS.fetch_add(1, Ordering::Relaxed);
+            if event_number < EVENT_LOG_LIMIT {
+                tracing::info!(
+                    event_number = event_number + 1,
+                    device_id = id,
+                    event = ?event,
+                    "Forwarding input event to injector"
+                );
+            }
             if let Err(e) = self.writer.send(Update::Event {id, event}).await {
                 tracing::warn!("Failed to send update to client {:?}", e);
             }
